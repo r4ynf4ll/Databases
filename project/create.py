6b094dba3282d820ddc052341b7ef679a322115e
Anna Buckel <abuckel0503@gmail.com>
2026-02-23 15:46:56 +0000

    league db made

--- project/create.py
+++ project/create.py
@@ -4,9 +4,11 @@ from sqlmodel import SQLModel, create_engine,Field
 
 engine = create_engine("sqlite:///league.db")
 
-df = pd.read_csv("LoL_champions.csv")
+df = pd.read_csv("champs.csv")
 
-df.to_sql('champions', con=engine, if_exists='replace', index=False)
+df.to_sql('champion_desc', con=engine, if_exists='replace', index=False)
 
 SQLModel.metadata.create_all(engine)
+
+
 
