4a8fb5078c2b492ef9f30bacb8821280cdda3af6
Anna Buckel <abuckel0503@gmail.com>
2026-02-23 15:14:37 +0000

    champions table filled

--- project/create.py
+++ project/create.py
@@ -1,14 +1,12 @@
+import pandas as pd
+
 from sqlmodel import SQLModel, create_engine,Field
 
-class Champion(SQLModel, table=True):
-    id: int | None = Field(default = None, primary_key = True)
-    name: str
-    tag1: str
-    tag2: str | None = None
-    roles: str
-    resource: str
-
 engine = create_engine("sqlite:///league.db")
+
+df = pd.read_csv("LoL_champions.csv")
+
+df.to_sql('champions', con=engine, if_exists='replace', index=False)
 
 SQLModel.metadata.create_all(engine)
 
